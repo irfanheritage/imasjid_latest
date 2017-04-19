dd1efde7095c6b21174609ca5169357cab2315f8
Ginanjar CK <ginanjarck@gmail.com>
2017-04-19 16:18:32 +0700

    fix error migration

--- db/migrate/20170413023609_add_contact_to_users.rb
+++ db/migrate/20170413023609_add_contact_to_users.rb
@@ -1,5 +1,5 @@
 class AddContactToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_reference :users, :contact, foreign_key: true, null: false
+    add_reference :users, :contact, foreign_key: true
   end
 end
